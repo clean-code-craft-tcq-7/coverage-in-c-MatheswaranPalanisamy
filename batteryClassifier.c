3cb3cd0b885057d013a2f9d83f4201ede1529553
MatheswaranPalanisamy <mathespbe@gmail.com>
2023-01-09 19:19:06 +0530

    Update batter classifier to get temperature limit for supported battery types

---
+++ batteryClassifier.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+#include "typewise-alert.h"
+#include "batteryClassifier.h"
+
+temperatureLimits BatteryTemperatureLimits[MAX_COOLING_TYPES] = 
+{
+    {
+        .lowerLimit = 0,
+        .upperLimit = 35,
+    },
+    {
+        .lowerLimit = 0,
+        .upperLimit = 45,
+    },
+    {
+        .lowerLimit = 0,
+        .upperLimit = 40,
+    },
+};
+
+temperatureLimits getTemperatureLimits(CoolingType coolingType)
+{
+    temperatureLimits temp_limits = {0, 0};
+    if(coolingType < MAX_COOLING_TYPES)
+    {
+        temp_limits.lowerLimit = BatteryTemperatureLimits[coolingType].lowerLimit;
+        temp_limits.upperLimit = BatteryTemperatureLimits[coolingType].upperLimit;
+    }
+
+    return temp_limits;
+}
