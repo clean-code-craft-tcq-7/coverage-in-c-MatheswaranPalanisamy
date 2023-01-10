f2f4a0dfcde0738a79e6de5277174b1a335153d1
MatheswaranPalanisamy <mathespbe@gmail.com>
2023-01-10 12:31:50 +0530

    Update alert to controller with testable alert message

--- controllerAlerter.c
+++ controllerAlerter.c
@@ -1,9 +1,25 @@
 #include <stdio.h>
+#include <string.h>
 #include "typewise-alert.h"
 #include "controllerAlerter.h"
 
-void sendToController(BreachType breachType) 
+const unsigned short header = 0xfeed;
+char alertMsg[MAX_CTRL_ALERT_MSG_LEN];
+
+static void getAlertMessage(BreachType breachType, char *msgBuff)
 {
-  const unsigned short header = 0xfeed;
-  printf("%x : %x\n", header, breachType);
+  if(breachType < MAX_BREACH_TYPES)
+  {
+    memset(alertMsg, 0, MAX_CTRL_ALERT_MSG_LEN);
+    sprintf(alertMsg, "%x : %x", header, breachType);
+    return alertMsg;
+  }
+
+  return NULL;
+}
+
+void sendToController(BreachType breachType, void (*alerter_func)(char *, char *)) 
+{
+  getAlertMessage(breachType, alertMsg);
+  alerter_func(NULL, alertMsg);
 }
