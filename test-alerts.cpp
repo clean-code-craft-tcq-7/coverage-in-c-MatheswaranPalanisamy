b4e91fa38892115f6f96549962fa09f72b28f49c
MatheswaranPalanisamy <mathespbe@gmail.com>
2023-01-10 12:34:39 +0530

    Update test cases for alerts, limit and breach classifiers

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -1,8 +1,86 @@
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
-
 #include "test/catch.hpp"
 #include "typewise-alert.h"
+#include "alerterMock.h"
 
-TEST_CASE("infers the breach according to limits") {
- // REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
+extern char *testDest;
+extern char *testMsg;
+
+TEST_CASE("Alert Controller for Normal") {
+  testDest = NULL;
+  testMsg = NULL;
+
+  alertBatteryStatus(TO_CONTROLLER, NORMAL, mockControllerAlerter);
+
+  REQUIRE(testDest == NULL);
+  REQUIRE(strcmp(testMsg, "0xfeed : 0x0") == 0);
+}
+
+TEST_CASE("Alert Controller for too low") {
+  testDest = NULL;
+  testMsg = NULL;
+
+  alertBatteryStatus(TO_CONTROLLER, TOO_LOW, mockControllerAlerter);
+
+  REQUIRE(testDest == NULL);
+  REQUIRE(strcmp(testMsg, "0xfeed : 0x1") == 0);
+}
+
+TEST_CASE("Alert Controller for too high") {
+  testDest = NULL;
+  testMsg = NULL;
+
+  alertBatteryStatus(TO_CONTROLLER, TOO_HIGH, mockControllerAlerter);
+
+  REQUIRE(testDest == NULL);
+  REQUIRE(strcmp(testMsg, "0xfeed : 0x2") == 0);
+}
+
+TEST_CASE("Alert email for too high") {
+  testDest = NULL;
+  testMsg = NULL;
+
+  alertBatteryStatus(TO_EMAIL, TOO_HIGH, mockEmailAlerter);
+
+  REQUIRE(strcmp(testDest, "a.b@c.com"));
+  REQUIRE(strcmp(testMsg, "Hi, the temperature is too high") == 0);
+}
+
+TEST_CASE("Alert email for too low") {
+  testDest = NULL;
+  testMsg = NULL;
+
+  alertBatteryStatus(TO_EMAIL, TOO_LOW, mockEmailAlerter);
+
+  REQUIRE(strcmp(testDest, "a.b@c.com"));
+  REQUIRE(strcmp(testMsg, "Hi, the temperature is too low") == 0);
+}
+
+TEST_CASE("No Alert by email for Normal") {
+  testDest = NULL;
+  testMsg = NULL;
+
+  alertBatteryStatus(TO_EMAIL, NORMAL, mockEmailAlerter);
+
+  REQUIRE(testDest == NULL);
+  REQUIRE(testMsg == NULL);
+}
+
+TEST_CASE("No Alert when alert type is invalid while breach is too low") {
+  testDest = NULL;
+  testMsg = NULL;
+
+  alertBatteryStatus(MAX_ALERTER_TARGETS, TOO_LOW, mockEmailAlerter);
+
+  REQUIRE(testDest == NULL);
+  REQUIRE(testMsg == NULL);
+}
+
+TEST_CASE("No Alert when alert type is invalid while breach is too high") {
+  testDest = NULL;
+  testMsg = NULL;
+
+  alertBatteryStatus(MAX_ALERTER_TARGETS, TOO_HIGH, mockEmailAlerter);
+
+  REQUIRE(testDest == NULL);
+  REQUIRE(testMsg == NULL);
 }
---
+++ test-breachClassifier.cpp
@@ -0,0 +1,48 @@
+#include "test/catch.hpp"
+#include "typewise-alert.h"
+#include "breachClassifier.h"
+
+TEST_CASE("too low in passive cooling") {
+  BreachType breachType = classifyTemperatureBreach(PASSIVE_COOLING, -5);
+  REQUIRE(breachType == TOO_LOW);
+}
+
+TEST_CASE("Normal in passive cooling") {
+  BreachType breachType = classifyTemperatureBreach(PASSIVE_COOLING, 1);
+  REQUIRE(breachType == NORMAL);
+}
+
+TEST_CASE("too High in passive cooling") {
+  BreachType breachType = classifyTemperatureBreach(PASSIVE_COOLING, 36);
+  REQUIRE(breachType == TOO_HIGH);
+}
+
+TEST_CASE("too low in High Active cooling") {
+  BreachType breachType = classifyTemperatureBreach(HI_ACTIVE_COOLING, -1);
+  REQUIRE(breachType == TOO_LOW);
+}
+
+TEST_CASE("Normal in High Active cooling") {
+  BreachType breachType = classifyTemperatureBreach(HI_ACTIVE_COOLING, 2);
+  REQUIRE(breachType == NORMAL);
+}
+
+TEST_CASE("too High in High Active cooling") {
+  BreachType breachType = classifyTemperatureBreach(HI_ACTIVE_COOLING, 46);
+  REQUIRE(breachType == TOO_HIGH);
+}
+
+TEST_CASE("too low in Medium Active cooling") {
+  BreachType breachType = classifyTemperatureBreach(MED_ACTIVE_COOLING, -3);
+  REQUIRE(breachType == TOO_LOW);
+}
+
+TEST_CASE("normal in Medium Active cooling") {
+  BreachType breachType = classifyTemperatureBreach(MED_ACTIVE_COOLING, 3);
+  REQUIRE(breachType == NORMAL);
+}
+
+TEST_CASE("too high in Medium Active cooling") {
+  BreachType breachType = classifyTemperatureBreach(MED_ACTIVE_COOLING, 41);
+  REQUIRE(breachType == TOO_HIGH);
+}
